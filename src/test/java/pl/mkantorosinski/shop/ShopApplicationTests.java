6cd15b5e343d8a499fffa13e3a1488a8938f9e7b
Mariusz <mkantoro@o2.pl>
2022-11-20 14:08:40 +0100

    Initial commit

---
+++ src/test/java/pl/mkantorosinski/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mkantorosinski.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
